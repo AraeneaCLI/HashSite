733fb7c054c545090ff01bd8950714c6b6cee555
Yash Mankar <82888014+AraeneaCLI@users.noreply.github.com>
2024-01-01 09:51:38 +0000

    Portfolio Main Changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Yash Mankar",
+  title: "Full Stack Developer",
+  email: "yash.mankar10122003@gmail.com",
+  gitHub: "AraeneaCLI",
+  instagram: "yash0.flac",
+  linkedIn: "yash-mankar-23b453269",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/banner6.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "QuickStruct",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    "Open Source Data Structure Command Line Interface Application",
+    url: "https://github.com/AraeneaCLI/QuickStruct",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Gradient Generator",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Contributed to Gradient Generator Module",
+    url: "https://github.com/AraeneaCLI/devstar",
   },
   {
-    title: "My Resume Site",
+    title: "Easify",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A To Do List Mobile Application, Soon to be Available on Desktop",
+    url: "https://github.com/AraeneaCLI/Easify-app",
   },
 ];
 
@@ -61,6 +55,7 @@ const Portfolio = () => {
     <section className="padding" id="portfolio">
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
@@ -70,9 +65,9 @@ const Portfolio = () => {
         </div>
         <div className="container">
           {projectList.map((project) => (
-            <div className="box" key={project.title}>
+            <div className="box" key={project.title} background-color="white">
               <a href={project.url} target="_blank" rel="noopener noreferrer">
-                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+                <h3 style={{ flexBasis: "20px" }}>{project.title}</h3>
               </a>
               <p className="small">{project.description}</p>
             </div>
